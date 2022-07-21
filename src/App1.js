abaaf15ca2836f9b3dfb6ac282ea1e04378c666e
AP <aprameya@hotmail.com>
2022-07-20 21:20:01 -0400

    Attempted to show a custom login screen, but still shows standard
    https://aws.amazon.com/blogs/mobile/amplify-uis-new-authenticator-component-makes-it-easy-to-add-customizable-login-pages-to-your-react-angular-or-vue-app/
    https://codesandbox.io/s/change-your-layout-rh4z9?file=/src/App.js
    https://github.com/swaminator/cra-starter

---
+++ src/App1.js
@@ -0,0 +1,72 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+
+
+import Amplify from "@aws-amplify/core";
+import { DataStore } from "@aws-amplify/datastore";
+
+import { Todo } from "./models";
+// import awsConfig from "./aws-exports";
+// Amplify.configure(awsConfig);
+// https://aws.amazon.com/blogs/mobile/amplify-uis-new-authenticator-component-makes-it-easy-to-add-customizable-login-pages-to-your-react-angular-or-vue-app/
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+// import React from 'react';
+Amplify.configure(awsExports);
+
+
+function onCreate() {
+  DataStore.save(
+    new Todo({
+      description: `Test todo ${Math.random().toString(36).substring(2, 5)}`
+    })
+  );
+}
+
+function App({ signOut, user }) {
+// function App() {
+  const [todos, setTodos] = useState ([]);
+
+  async function getTodos() {
+    const alltodos = await DataStore.query(Todo)
+    setTodos(alltodos)
+  }
+
+  useEffect(() => { 
+    getTodos()
+    const subscription = DataStore.observe(Todo).subscribe(() => getTodos())
+    return () => subscription.unsubscribe()
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <input type="button" value="NEW - SKIP BACKEND" onClick={() => { onCreate(); getTodos(setTodos)} } />
+      </div>
+      <table border="1">
+        <thead>
+          <tr><td>Id</td><td>Desc</td></tr>
+        </thead>
+        <tbody>
+          {todos.map( (item,i) => {
+            return <tr key={i}><td>{todos[i].id.substring(0,8)}...</td><td>{todos[i].description}</td></tr>
+          } )}
+        </tbody>
+      </table>
+      </header>
+      
+      <div>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
